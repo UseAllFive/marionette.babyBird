16b426e2d5dcd87c71bbd2677723d2fb6b3be63c
Justin Anastos <janastos@useallfive.com>
2014-06-23 16:09:05 -0400

    Origination.

---
+++ Gruntfile.js
@@ -0,0 +1,89 @@
+var config;
+var matchdep = require('matchdep');
+var module;
+
+config = {
+    files: {
+        build: 'src/marionette.babyBird.js',
+        check: [
+            'src/marionette.babyBird.js',
+            'Gruntfie.js'
+        ],
+        dest: 'dist/marionette.babyBird.min.js',
+        docs: 'docs'
+    }
+};
+
+module.exports = function(grunt) {
+
+    var pkg = grunt.file.readJSON('package.json');
+    var defaultTasks;
+
+    grunt.initConfig({
+        pkg: pkg,
+
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            all: {
+                files: {
+                    src: config.files.check
+                }
+            }
+        },
+
+        jscs: {
+            all: {
+                files: {
+                    src: config.files.check
+                }
+            }
+        },
+
+        groc: {
+            options: {
+                out: config.files.docs,
+                strip: 'src/'
+            },
+            all: {
+                src: config.files.build
+            },
+            github: {
+                options: {
+                    github: true
+                },
+                src: config.files.build
+            }
+        },
+
+        uglify: {
+            all: {
+                src: config.files.build,
+                dest: config.files.dest
+            }
+        },
+
+        watch: {
+            options: {
+                interrupt: true
+            },
+            js: {
+                files: config.files.check,
+                tasks: ['uglify']
+            }
+        }
+    });
+
+    // Load all npm dependencies.
+    matchdep.filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // Register tasks.
+    defaultTasks = [
+        'jshint',
+        'jscs',
+        'uglify'
+    ];
+    grunt.registerTask('default', defaultTasks);
+    grunt.registerTask('dev', defaultTasks.concat('watch'));
+};
--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-marionette.babyBird
-===================
+# marionette.babyBird
 
-Recursively run toJSON on deeply nested models/collections for Marionette views to provide a plain JavaScript object to templates.
+Flatten deeply nested Backbone [models](http://backbonejs.org/#Model) and [collections](http://backbonejs.org/#Collection) into a plain JavaScript object when using built-in [Marionette.Renderer](https://github.com/marionettejs/backbone.marionette/blob/master/docs/marionette.renderer.md).
+
+## To Use
+Include this file before your application attempts to render any templates.
+
+## Dependencies
+This module requires [Backbone](http://backbonejs.org), [Marionette](http://marionettejs.com/), and [Underscore](http://underscorejs.org/).
+
+## Limitations
+There is no checking done to catch circular dependencies.
+
+## Updating Annotated Source Code
+To update the annotated source code, you must install Pygments on your system
+as it is a dependency of `groc`.
+
+```
+sudo pip install Pygments
+```
